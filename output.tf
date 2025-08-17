fc04d6bd7a7fb516ff4c11c8630129cf7af18833
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-08-17 17:18:33 +0530

    committed

--- output.tf
+++ output.tf
@@ -1,3 +1,27 @@
-output "id" {
-  value       = aws_security_group.main.id
+output "sg_id" {
+    value = aws_security_group.main.id
 }
+
+output "mysql_sg_id" {
+    value = aws_security_group.main.id
+} 
+
+output "backend_sg_id" {
+    value = aws_security_group.main.id
+} 
+
+output "frontend_sg_id" {
+    value = aws_security_group.main.id
+} 
+
+output "bastion_sg_id" {
+    value = aws_security_group.main.id
+}
+
+output "ansible_sg_id" {
+    value = aws_security_group.main.id
+} 
+
+output "app_alb_sg_id" {
+    value = aws_security_group.main.id
+} 
